726aeef14800ab1f24f958fe27958e1c2983fb32
alberttech:) <albikakovacs23@gmail.com>
2026-08-14 21:18:12 +0200

    Refactor DVD logo display code in dvd.c

--- dvd.c
+++ dvd.c
@@ -1,125 +1,140 @@
 #include <SDL3/SDL.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
 
+int main(void)
+{
+    const int window_width = 800;
+    const int window_height = 600;
 
-int main() {
-    time_t t;
-    SDL_Init(SDL_INIT_VIDEO);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
     SDL_Surface *surface = SDL_LoadBMP("dvd-logo.bmp");
 
-    int logo_width = surface->w;
-    int logo_heght = surface->h;
+    if (surface == NULL) {
+        SDL_Log("Failed to load image: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-    int window_width = 800;
-    int window_heght = 600;
+    const int logo_width = surface->w;
+    const int logo_height = surface->h;
 
-    SDL_Window *window = SDL_CreateWindow(   //this is gonna creat a window
+    SDL_Window *window = SDL_CreateWindow(
         "DVD",
-        window_width, 
-        window_heght,
+        window_width,
+        window_height,
         0
     );
 
+    if (window == NULL) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        SDL_DestroySurface(surface);
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+    if (renderer == NULL) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroySurface(surface);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_DestroySurface(surface);
-    if (surface == NULL) {
-        SDL_Log("Failed to load image: %s", SDL_GetError());
+
+    if (texture == NULL) {
+        SDL_Log("Failed to create texture: %s", SDL_GetError());
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
 
-
-    if (texture == NULL) {
-    SDL_Log("Failed to create texture: %s", SDL_GetError());
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 1;
-    }
-
-    bool running = true;
-    SDL_Event event;
-
-    float x = 300;
-    float y = 200;
-
-    float dx = 0.1;
-    float dy = 0.1; 
-
-    srand(time(&t));        //this is the random color picker
-    int colors[][3] = {
+    const int colors[][3] = {
         {255, 0, 0},
         {0, 255, 0},
         {0, 0, 255},
         {255, 255, 0},
         {255, 255, 255}
     };
-    int num_colors = sizeof(colors) / sizeof(colors[0]);
-    int random_colors = rand() % num_colors;
-    int number;
 
-    while (running) {      //this is the main func
+    const int num_colors = sizeof(colors) / sizeof(colors[0]);
+
+    srand((unsigned int)time(NULL));
+
+    bool running = true;
+    SDL_Event event;
+
+    float x = 300.0f;
+    float y = 200.0f;
+
+    float dx = 0.1f;
+    float dy = 0.1f;
+
+    while (running) {
 
         while (SDL_PollEvent(&event)) {
-
             if (event.type == SDL_EVENT_QUIT) {
                 running = false;
             }
-            
         }
 
         x += dx;
         y += dy;
 
-        if ( x <= 0 || x + logo_width >= window_width ) {
+        if (x <= 0 || x + logo_width >= window_width) {
             dx = -dx;
-            
-        int color = rand() % num_colors;
 
-        SDL_SetTextureColorMod(
-            texture,
-            colors[color][0],
-            colors[color][1],
-            colors[color][2]
-        );
-    }
-        
+            int color = rand() % num_colors;
 
-        if ( y <= 0 || y + logo_heght >= window_heght ) {
+            SDL_SetTextureColorMod(
+                texture,
+                colors[color][0],
+                colors[color][1],
+                colors[color][2]
+            );
+        }
+
+        if (y <= 0 || y + logo_height >= window_height) {
             dy = -dy;
 
-        int color = rand() % num_colors;
+            int color = rand() % num_colors;
 
-        SDL_SetTextureColorMod(
-            texture,
-            colors[color][0],
-            colors[color][1],
-            colors[color][2]
-        );
-    }
-
+            SDL_SetTextureColorMod(
+                texture,
+                colors[color][0],
+                colors[color][1],
+                colors[color][2]
+            );
+        }
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
-        SDL_FRect dst = { x, y, (float)logo_width,(float)logo_heght };
+        SDL_FRect dst = {
+            x,
+            y,
+            (float)logo_width,
+            (float)logo_height
+        };
 
         SDL_RenderTexture(renderer, texture, NULL, &dst);
-
         SDL_RenderPresent(renderer);
-        }
-SDL_DestroyTexture(texture);
-SDL_DestroyRenderer(renderer);
-SDL_DestroyWindow(window);
+    }
 
-SDL_Quit();
-return 0;
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
